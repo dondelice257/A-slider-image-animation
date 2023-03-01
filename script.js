65efe7c8a6526f253663a122e3e101e74c3261a2
Don Delice Dushime <107407612+dondelice257@users.noreply.github.com>
2023-03-01 23:25:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
         var star3 = document.getElementById('star3');
         var star4 = document.getElementById('star4');
         var star5 = document.getElementById('star5');
-        var pres = document.getElementById('pres9');
+        var pres = document.getElementById('pres10');
         var names = []
 
        
